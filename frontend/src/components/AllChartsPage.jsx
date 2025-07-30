7f47bb0f3b7956189f3e95c91473d2e67562ea04
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-30 16:05:21 -0500

    feat: add chart gallery page

---
+++ frontend/src/components/AllChartsPage.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import WeeklySummaryCard from "./WeeklySummaryCard";
+import StreakFlame from "./StreakFlame";
+import TemperatureChart from "./TemperatureChart";
+import WeatherChart from "./WeatherChart";
+import KPIGrid from "./KPIGrid";
+import FitnessScoreDial from "./FitnessScoreDial";
+import WeeklyRingsDashboard from "./WeeklyRingsDashboard";
+import AnalysisSection from "./AnalysisSection";
+
+export default function AllChartsPage() {
+  return (
+    <div className="p-6 space-y-6">
+      <WeeklySummaryCard>
+        <StreakFlame />
+      </WeeklySummaryCard>
+      <div className="grid gap-6 sm:grid-cols-2">
+        <TemperatureChart />
+        <WeatherChart />
+      </div>
+      <KPIGrid />
+      <FitnessScoreDial />
+      <div className="flex justify-center">
+        <WeeklyRingsDashboard />
+      </div>
+      <AnalysisSection />
+    </div>
+  );
+}
