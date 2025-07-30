a0e8c0554ccaa655e1d198e735043406556989b8
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-29 21:55:29 -0500

    Add correlation charts for activity metrics

--- frontend/src/components/AnalysisSection.jsx
+++ frontend/src/components/AnalysisSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DemoCharts from "./DemoCharts/DemoCharts";
+import CorrelationMap from "./CorrelationMap";
 
 export default function AnalysisSection() {
   return (
     <div className="space-y-10">
       <DemoCharts />
+      <CorrelationMap />
     </div>
   );
 }
