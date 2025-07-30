4e2d5d2c385c7ef92370bee3f7638eb54a3b6ee8
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-29 21:56:30 -0500

    Add Day/Night activity split donut chart

--- frontend/src/components/AnalysisSection.jsx
+++ frontend/src/components/AnalysisSection.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import DemoCharts from "./DemoCharts/DemoCharts";
+import DayNightSplitChart from "./DayNightSplitChart";
 
 export default function AnalysisSection() {
   return (
     <div className="space-y-10">
+      <DayNightSplitChart />
       <DemoCharts />
     </div>
   );
