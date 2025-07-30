f3c8d278bbdc6c7540db1966a488049e30f3708d
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-29 21:57:24 -0500

    Add weather impact bubble chart

--- frontend/src/components/AnalysisSection.jsx
+++ frontend/src/components/AnalysisSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DemoCharts from "./DemoCharts/DemoCharts";
+import WeatherImpactBubbleChart from "./WeatherImpactBubbleChart";
 
 export default function AnalysisSection() {
   return (
     <div className="space-y-10">
       <DemoCharts />
+      <WeatherImpactBubbleChart />
     </div>
   );
 }
